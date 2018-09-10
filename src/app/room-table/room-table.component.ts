e07bf5654bee75545ed57292a799dcb69bad5390
dudap1 <39228050+dudap1@users.noreply.github.com>
2018-09-10 14:33:42 +0200

    refreshData changes

--- src/app/room-table/room-table.component.ts
+++ src/app/room-table/room-table.component.ts
@@ -18,7 +18,7 @@ export class RoomTableComponent implements OnInit {
   sub = null;
 
   displayData = [];
-
+players=[];
   countRounds = [];
 
   password = localStorage.password;
@@ -35,18 +35,86 @@ export class RoomTableComponent implements OnInit {
 
   }
 
+
+  handleResult(result: Array<RecordResult>) {
+
+    let displayData = []
+    /*lista graczy*/
+    let players = [];
+    result.forEach(element => {
+      if (players.indexOf(element.login) == -1) {
+        players.push(element.login);
+       // displayData[players.indexOf(element.login)] = [];
+      }
+    });
+    this.players = players;
+
+    /*preparowanie rekordów kolejek*/
+    /*displayData powinno wygladac jakos tak*/
+    /*[
+        [RecordResult,RecordResult,RecordResult...],
+        [RecordResult,RecordResult...],
+        [RecordResult,RecordResult...]
+    ]*/
+    /*jak jest zaczecie meczykow to powinno wygladac tak*/
+    /*[
+        [RecordResult],
+        [null],
+        [null]
+    ]*/
+    let temp_dis_data: Array<Array<RecordResult>> = [];
+    result.forEach(element => {
+      temp_dis_data[this.players.indexOf(element.login)]
+        .push(element.id == null ? null : element);
+    })
+    temp_dis_data.forEach((player, playerIndex) => {
+      player.forEach((element, index) => {
+        if (!displayData[index]) {
+          displayData[index] = [];
+        }
+        displayData[index][playerIndex] = element;
+      })
+    })
+    this.displayData = displayData;
+
+
+    /* to jest na koncu do wpisania nowej kolejki */
+    this.newQueue = [];
+    this.players.forEach((element, i) => {
+      this.newQueue[i] = result[i];
+    })
+
+    // for (let i = 0; i < this.displayData.length; i++) {
+    //     this.newQueue.push({
+    //         id: this.displayData[i].id,
+    //         amount: null
+    //     })
+    // }
+
+    /*ustawienie najdłuższej kolejki*/
+    this.countRounds = [];
+    for (let i = 0; i < this.displayData.length; i++) {
+      if (this.countRounds.length < this.displayData[i].length) {
+        this.countRounds = this.displayData[i];
+      }
+    }
+
+  }
+
   refreshData() {
 
-    this.http.post('https://edarter2.herokuapp.com/api/getRoundsByName', {contestName: this.name, login: this.UserService._login})
-      .subscribe((res:Array<any> )=> {
+    this.http.post(`https://edarter2.herokuapp.com/api/getRoundsByName?contestName=${this.name}&login=${this.UserService._login}`,null)
+      .subscribe((res:Array<RecordResult> )=> {
+
         console.log(res);
   this.displayData  = res;
+  this.handleResult(res);
         },
         error => {
           console.error(error);
         })
 
-
+/*
     this.newQueue = [];
     for (let i = 0; i < this.displayData.length; i++) {
       this.newQueue.push({
@@ -55,15 +123,17 @@ export class RoomTableComponent implements OnInit {
       })
     }
 
+
     this.countRounds = [];
     for (let i = 0; i < this.displayData.length; i++) {
       this.countRounds = this.countRounds.length < this.displayData[i].rounds.length ? this.displayData[i].rounds : this.countRounds;
     }
+*/
   }
 
   addQueue() {
     for (let i = 0; i < this.newQueue.length - 1; i++) {
-      this.http.post(`https://edarter2.herokuapp.com/api/api/setRound?amount=${this.newQueue[i].pkt}&photoPath=&contest=${this.roomId}&player=${this.newQueue[i].id}`, null).subscribe(
+      this.http.post(`https://edarter2.herokuapp.com/api/setRound?amount=${this.newQueue[i].pkt}&photoPath=&contest=${this.roomId}&player=${this.newQueue[i].id}`, null).subscribe(
         res => {
           this.refreshData();
         },
@@ -76,7 +146,7 @@ export class RoomTableComponent implements OnInit {
   }
 
   update(id, nowailosc, roomId, idgracza) {
-    this.http.post(`https://edarter2.herokuapp.com/api/api/updateRound?id=${id}&amount=${nowailosc}&contest=${roomId}&player=${idgracza}`, null).subscribe(
+    this.http.post(`https://edarter2.herokuapp.com/api/updateRound?id=${id}&amount=${nowailosc}&contest=${roomId}&player=${idgracza}`, null).subscribe(
       res => {
         this.refreshData();
       },
@@ -88,7 +158,7 @@ export class RoomTableComponent implements OnInit {
 
   deleteQueue() {
     const id = this.displayData[0].rounds[this.displayData[0].rounds.length - 1].id
-    this.http.post(`https://edarter2.herokuapp.com/api/api/deleteRound?id=${id}`, null).subscribe(
+    this.http.post(`https://edarter2.herokuapp.com/api/deleteRound?id=${id}`, null).subscribe(
       res => {
         this.refreshData();
       },
@@ -100,9 +170,9 @@ export class RoomTableComponent implements OnInit {
   }
 
   joinMatch() {
-    this.http.post(`https://edarter2.herokuapp.com/api/api/joinMatch?contest_name=${this.name}&contest_pass=${this.password}&login=${this.login}`, null).subscribe(
+    this.http.post(`https://edarter2.herokuapp.com/api/joinContest?contest_name=${this.name}&contest_pass=${this.password}&login=${this.login}`, null).subscribe(
       res => {
-        Utils.showNotification('Utworzono konto', 'success')
+        Utils.showNotification('Dodano gracza', 'success')
       },
       error1 => {
         console.error(error1);
@@ -114,4 +184,12 @@ export class RoomTableComponent implements OnInit {
     this.refreshData();
   }
 
+}
+
+class RecordResult {
+  id: number;
+  amount: number;
+  fullAmount: number;
+  photoPath: string;
+  login: string;
 }
